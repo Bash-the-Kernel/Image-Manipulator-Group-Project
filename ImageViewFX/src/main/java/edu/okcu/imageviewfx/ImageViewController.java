801d1430c5ef60ce3538c108fe85d6b09d62a05b
Jack <0319024@schoolsnet.act.edu.au>
2023-02-13 15:23:54 -0600

    Added Grayscale
    button turns image into grayscale

--- ImageViewFX/src/main/java/edu/okcu/imageviewfx/ImageViewController.java
+++ ImageViewFX/src/main/java/edu/okcu/imageviewfx/ImageViewController.java
@@ -1,10 +1,8 @@
 package edu.okcu.imageviewfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import javafx.scene.image.*;
 
 import java.io.File;
 
@@ -23,7 +21,27 @@ public class ImageViewController {
         picture.setImage(image);
     }
     @FXML
-    protected void onDuplicateButtonClick() {
-        picture1.setImage(picture.getImage());
+    protected void onGrayScaleButtonClick() {
+        Image image = picture.getImage();
+        PixelReader pixelReader = image.getPixelReader();
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        WritableImage grayscaleImage = new WritableImage(width, height);
+        PixelWriter pixelWriter = grayscaleImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+
+                int color = pixelReader.getArgb(x, y);
+                int red = (color >> 16) & 0xff;
+                int green = (color >> 8) & 0xff;
+                int blue = color & 0xff;
+                int gray = 255 - (red + green + blue) / 3;
+                int newColor = (gray << 16) | (gray << 8) | gray;
+                pixelWriter.setArgb(x, y, -newColor);
+            }
+        }
+        picture1.setImage(grayscaleImage);
     }
 }
